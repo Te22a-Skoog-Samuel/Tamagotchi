192fab652508a6d65584ab019b765b70fd363377
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-09-10 11:14:26 +0200

    yjgyugfkugk

---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,19 @@
+class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List<string> words;
+    private bool isAlive;
+    private Random generator;
+    public string name;
+
+    
+
+
+
+
+
+
+
+
+}
